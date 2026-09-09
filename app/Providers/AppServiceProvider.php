66d5d4e5a5a2cae188dd47179e381cb62c3b8f10
Houss35Code <h.halifa035@gmail.com>
2026-09-09 22:11:46 +0200

    Fix routing for subdirectory and update back link

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         \Carbon\Carbon::setLocale('fr');
+
+        if (config('app.env') === 'production') {
+            URL::forceRootUrl(config('app.url'));
+        }
     }
 }
