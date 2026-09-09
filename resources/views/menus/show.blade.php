89a639b1718db81dec11810df2a166e48aef878c
Houss35Code <h.halifa035@gmail.com>
2026-09-09 23:30:42 +0200

    Fix PDF embed click blocking and enforce absolute back URL

--- resources/views/menus/show.blade.php
+++ resources/views/menus/show.blade.php
@@ -5,16 +5,17 @@
 
 @section('content')
     <div class="menu-detail">
-        <a href="{{ route('home') }}#menus" class="menu-detail__back">
+        <a href="https://trailbreizh.alwaysdata.net/madamepetitplat/#menus" class="menu-detail__back" style="position: relative; z-index: 100; display: inline-block;">
             ← Retour aux menus
         </a>
         <h1 class="menu-detail__title">
             {{ $menu->titre }}
         </h1>
-        <embed
+        <iframe
             class="menu-detail__embed"
             src="{{ asset('storage/' . $menu->fichier) }}"
             type="application/pdf"
-        >
+            style="width: 100%; height: 800px; border: none;"
+        ></iframe>
     </div>
 @endsection
