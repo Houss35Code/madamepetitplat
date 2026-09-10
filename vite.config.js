5c98c848d94db7bdfc85980aa3a1c56ccac23b46
Houss35Code <h.halifa035@gmail.com>
2026-09-10 21:00:18 +0200

    Correction du chemin base Vite pour le sous-dossier et ajout du build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/madamepetitplat/build/',
     plugins: [
         laravel({
             input: ['resources/sass/main.scss', 'resources/js/app.js'],
